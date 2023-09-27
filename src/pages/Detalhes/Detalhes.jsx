fb5092277e3e2b16fa48e503f4279d2581c6ac24
JoaoAlmir <joaoalmir@alu.ufc.br>
2023-09-27 17:45:12 -0300

    formato da data alterado

--- src/pages/Detalhes/Detalhes.jsx
+++ src/pages/Detalhes/Detalhes.jsx
@@ -6,6 +6,7 @@ import { FaStar, FaCalendar } from 'react-icons/fa';
 
 import "./style.css"
 import { useParams } from "react-router-dom";
+import moment from "moment";
 export const Detalhes = () => {
 
 
@@ -34,7 +35,7 @@ export const Detalhes = () => {
             <div className="detalhes-container .d-flex flex-column align-items-center">
                 <Header />
                 <h1>{movie.original_title}</h1>
-                <h5>{movie.release_date}</h5>
+                <h5>{moment(movie.release_date).format('DD/MM/YYYY')}</h5>
                 <img className="detalhes-icon" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
                 <link rel="icon" href="./assets/icone_logo_aba.png" />
                 <h5 className="text-warning " >{movie.vote_average}
